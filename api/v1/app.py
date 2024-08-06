8e667722f01bd2cb8a859f7bd22a2667a5682e4e
AbanoubAziz <abanoubsamy2341@gmail.com>
2024-08-06 06:49:43 +0300

    doc

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,7 +8,6 @@ from flask import Flask, Blueprint, jsonify, make_response
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
