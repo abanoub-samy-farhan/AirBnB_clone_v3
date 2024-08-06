7e6c93e8367ea92c90e32ec5dd30b25fcb4c004d
AbanoubAziz <abanoubsamy2341@gmail.com>
2024-08-06 04:34:56 +0300

    #3 Docs added

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Doced"""
 import os
 from flask import Flask
 from models import storage
@@ -7,7 +8,13 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+@app.teardown_appcontext
+def teardown(exception):
+    """Doced"""
+    storage.close()
+
 if __name__ == "__main__":
+    """Doced"""
     host = "0.0.0.0"
     port = "5000"
 
