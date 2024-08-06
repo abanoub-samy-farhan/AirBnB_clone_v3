35734144025e8264c98e3ba8a4566200c7e6f143
AbanoubAziz <abanoubsamy2341@gmail.com>
2024-08-06 06:49:29 +0300

    doc

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
-"""Doced"""
+"""app.py to connect to API"""
 import os
-from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from flask import Flask, Blueprint, jsonify, make_response
 
-"""Doced"""
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
 
 @app.teardown_appcontext
-def teardown(exception):
-    """Doced"""
+def teardown_appcontext(code):
+    """teardown_appcontext"""
     storage.close()
 
 if __name__ == "__main__":
-    """Doced"""
-    host = "0.0.0.0"
-    port = "5000"
-
-    if os.getenv("HBNB_API_HOST") is not None:
-        host = os.getenv("HBNB_API_HOST")
-    if os.getenv("HBNB_API_PORT") is not None:
-        port = os.getenv("HBNB_API_PORT")
-        
-    app.run(debug=True, threaded=True, host=host, port=port)
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
