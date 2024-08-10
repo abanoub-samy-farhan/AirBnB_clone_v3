4555821f76ee390fb3ffb1089e52933995f52e92
AbanoubAziz <abanoubsamy2341@gmail.com>
2024-08-10 15:03:32 +0300

    States Flask fixs

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -49,7 +49,7 @@ def states_id_put(state_id):
         abort(404)
     if not request.get_json():
         return make_response(jsonify({"error":"Not a JSON"}), 400)
-    for key, val in request.get_json().items()
+    for key, val in request.get_json().items():
         if key not in ["id", "created_at", "updated_at"]:
             setattr(state, key, val)
     state.save()
